2009224c824db00a307b21234a5670d71b50e1ec
Gabriel Romanini de Barros <gabriel.romanini.barros@gmail.com>
2025-02-17 19:05:14 -0500

    Cleaned up server code

--- src/server.rs
+++ src/server.rs
@@ -6,7 +6,6 @@ use tokio::select;
 use sqlx::postgres::PgPoolOptions;
 use dotenv::dotenv;
 
-use rustls::{Certificate, PrivateKey, ServerConfig};
 mod routes;
 use crate::routes::auth;
 use axum::routing::post;
@@ -14,24 +13,6 @@ use axum::routing::get;
 use axum::routing::Router;
 use axum::response::Html;
 
-/*
-fn load_certs(path: &str) -> Vec<Certificate> {
-    let cert_file = File::open(path).expect("Failed to open certificate file");
-    let mut reader = BufReader::new(cert_file);
-    rustls_pemfile::certs(&mut reader)
-        .unwrap()
-        .iter()
-        .map(|v| Certificate(v.clone()))
-        .collect()
-}
-
-fn load_private_key(path: &str) -> PrivateKey {
-    let key_file = File::open(path).expect("Failed to open private key file");
-    let mut reader = BufReader::new(key_file);
-    let keys = rustls_pemfile::pkcs8_private_keys(&mut reader).unwrap();
-    PrivateKey(keys[0].clone())
-}
-*/
 struct ServerState {
     db: sqlx::PgPool,
     tx: broadcast::Sender<String>,
@@ -41,20 +22,10 @@ struct ServerState {
 async fn main() {
     dotenv().ok();
 
-    // TLS configuration
-    //let certs = load_certs("/etc/letsencrypt/live/ws.gchat.cloud/fullchain.pem");
-    //let key = load_private_key("/etc/letsencrypt/live/ws.gchat.cloud/privkey.pem");
 
     let db_url = std::env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
 
-    /*
-    let config = ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_single_cert(certs, key)
-        .expect("Failed to create TLS config");
-    */
 
     // DB connection pool
     let pool = PgPoolOptions::new()
@@ -63,7 +34,6 @@ async fn main() {
         .await
         .expect("Failed to create pool");
 
-    //let acceptor = TlsAcceptor::from(std::sync::Arc::new(config));
 
     let server = TcpListener::bind("0.0.0.0:3012").await.unwrap();
     let(tx, mut _rx) = broadcast::channel::<String>(100);
@@ -85,13 +55,10 @@ async fn main() {
 async fn handle_websocket_connections (server: TcpListener, tx: broadcast::Sender<String>, state: std::sync::Arc<ServerState>) {
 
     while let Ok((stream, _addr)) = server.accept().await {
-        //let acceptor = acceptor.clone();
         let tx_clone = tx.clone();
         let state = state.clone(); // Lightweight since state is wrapped in Arc
 
         tokio::spawn(async move {
-            //let tls_stream = acceptor.accept(stream).await.unwrap();
-            //let mut websocket = accept_async(tls_stream).await.unwrap();
             let mut websocket = accept_async(stream).await.unwrap();
             let mut rx = tx_clone.subscribe();
 
