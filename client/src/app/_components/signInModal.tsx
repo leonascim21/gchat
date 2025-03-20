89c229253ba640a376ea16588ee6b42f5a902f20
leonascim <leonascim21@gmail.com>
2025-03-20 16:16:25 -0400

    eslint fix

--- client/src/app/_components/signInModal.tsx
+++ client/src/app/_components/signInModal.tsx
@@ -12,6 +12,7 @@ import { Label } from "@/components/ui/label";
 import { z } from "zod";
 import { useForm } from "@tanstack/react-form";
 import axios from "axios";
+import qs from "qs";
 
 const userSchema = z.object({
   username: z.string().min(1, "Username is required"),
@@ -24,7 +25,6 @@ interface Props {
 
 export default function SignInModal({ showSignUp }: Props) {
   const [open, setOpen] = useState(true);
-  const qs = require("qs");
 
   const form = useForm({
     defaultValues: {
@@ -43,16 +43,20 @@ export default function SignInModal({ showSignUp }: Props) {
         );
         console.log("Login successful:", response.data);
         localStorage.setItem("token", response.data.token);
-      } catch (error: any) {
-        console.error("Registration failed:", error);
-        if (error.response) {
-          console.error("Server response:", error.response.data);
-          console.error("Server status:", error.response.status);
-          console.error("Server headers:", error.response.headers);
-        } else if (error.request) {
-          console.error("No response received:", error.request);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          console.error("Registration failed:", error);
+          if (error.response) {
+            console.error("Server response:", error.response.data);
+            console.error("Server status:", error.response.status);
+            console.error("Server headers:", error.response.headers);
+          } else if (error.request) {
+            console.error("No response received:", error.request);
+          } else {
+            console.error("Error setting up the request:", error.message);
+          }
         } else {
-          console.error("Error setting up the request:", error.message);
+          console.error("An unexpected error occurred:", error);
         }
       }
     },
@@ -81,17 +85,44 @@ export default function SignInModal({ showSignUp }: Props) {
           }}
         >
           <div className="flex flex-col gap-4">
-            <form.Field
-              name="username"
-              children={(field) => {
-                return (
+            <form.Field name="username">
+              {(field) => (
+                <div className="grid gap-2">
+                  <Label htmlFor={field.name}>Username</Label>
+                  <div>
+                    <Input
+                      id={field.name}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      value={field.state.value}
+                    />
+                    {field.state.meta.isDirty && field.state.meta.errors && (
+                      <p className="text-red-500 text-sm">
+                        {field.state.meta.errors[0]?.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+              )}
+            </form.Field>
+            <form.Field name="password">
+              {(field) => (
+                <div className="flex flex-col gap-6">
                   <div className="grid gap-2">
-                    <Label htmlFor={field.name}>Username</Label>
+                    <div className="flex justify-between items-center">
+                      <Label htmlFor={field.name}>Password</Label>
+                      <a
+                        href="#"
+                        className="text-sm underline-offset-4 hover:underline"
+                      >
+                        Forgot your password?
+                      </a>
+                    </div>
                     <div>
                       <Input
                         id={field.name}
                         onChange={(e) => field.handleChange(e.target.value)}
                         value={field.state.value}
+                        type="password"
                       />
                       {field.state.meta.isDirty && field.state.meta.errors && (
                         <p className="text-red-500 text-sm">
@@ -100,51 +131,18 @@ export default function SignInModal({ showSignUp }: Props) {
                       )}
                     </div>
                   </div>
-                );
-              }}
-            />
-            <form.Field
-              name="password"
-              children={(field) => {
-                return (
-                  <div className="flex flex-col gap-6">
-                    <div className="grid gap-2">
-                      <div className="flex justify-between items-center">
-                        <Label htmlFor={field.name}>Password</Label>
-                        <a
-                          href="#"
-                          className="text-sm underline-offset-4 hover:underline"
-                        >
-                          Forgot your password?
-                        </a>
-                      </div>
-                      <div>
-                        <Input
-                          id={field.name}
-                          onChange={(e) => field.handleChange(e.target.value)}
-                          value={field.state.value}
-                          type="password"
-                        />
-                        {field.state.meta.isDirty &&
-                          field.state.meta.errors && (
-                            <p className="text-red-500 text-sm">
-                              {field.state.meta.errors[0]?.message}
-                            </p>
-                          )}
-                      </div>
-                    </div>
-                  </div>
-                );
-              }}
-            />
+                </div>
+              )}
+            </form.Field>
             <form.Subscribe
               selector={(state) => [state.canSubmit, state.isSubmitting]}
-              children={([canSubmit, isSubmitting]) => (
+            >
+              {([canSubmit, isSubmitting]) => (
                 <Button type="submit" disabled={!canSubmit}>
                   {isSubmitting ? "..." : "Sign In"}
                 </Button>
               )}
-            />
+            </form.Subscribe>
           </div>
         </form>
         <div className="mt-4 text-center text-sm">
