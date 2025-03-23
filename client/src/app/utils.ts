70db5bac2b6691cb98c523c13ba5f4f81c2063dd
leonascim <leonascim21@gmail.com>
2025-03-23 00:32:10 -0400

    utils

--- client/src/app/utils.ts
+++ client/src/app/utils.ts
@@ -50,3 +50,19 @@ export function generateProfilePictureSVG() {
   const base64Svg = btoa(svg);
   return `data:image/svg+xml;base64,${base64Svg}`;
 }
+
+export function getIdFromJWT() {
+  const token = localStorage.getItem("token") ?? "";
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return parseInt(JSON.parse(jsonPayload).sub);
+}
