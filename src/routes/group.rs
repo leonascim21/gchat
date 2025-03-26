0d1237bf2c085e89a453935b39e65eaf29fde205
leonascim <leonascim21@gmail.com>
2025-03-26 12:14:28 -0400

    add group/get-users route to get users in a group

--- src/routes/group.rs
+++ src/routes/group.rs
@@ -19,6 +19,7 @@ pub fn router() -> Router<Arc<ServerState>> {
     Router::new()
         .route("/create", post(create_group_chat))
         .route("/get", get(get_groups))
+        .route("/get-users", get(get_users_in_group))
 }
 
 
@@ -130,6 +131,69 @@ async fn get_groups(
         }
         Err(_) => {
             (StatusCode::INTERNAL_SERVER_ERROR,Json(json!({ "error": "Failed to fetch friendships" }))).into_response()
+        }
+    }
+}
+
+async fn get_users_in_group(    
+    State(state): State<Arc<ServerState>>,
+    Query(params): Query<HashMap<String, String>>
+) -> impl IntoResponse  {
+    let token = match params.get("token") {
+        None => {
+            return (StatusCode::UNAUTHORIZED, Json(json!({ "error": "Missing token" }))).into_response()
+        }
+        Some(token) => token,
+    };
+    let jwt_key = std::env::var("JWT_KEY").expect("JWT_KEY must be set");
+    let user_id = match validate_token(token, jwt_key).await {
+        Ok(claims) => claims.sub.parse::<i32>().unwrap(),
+        Err(_) => {
+            return (StatusCode::UNAUTHORIZED,Json(json!({ "error": "Invalid token" }))).into_response()
+        }
+    };
+    let group_id = match params.get("group_id") {
+        None => {
+            return (StatusCode::BAD_REQUEST, Json(json!({ "error": "Missing group_id" }))).into_response()
+        }
+        Some(group_id) => group_id,
+    };
+    let group_id = group_id.parse::<i32>().unwrap();
+    let users = sqlx::query!(
+        r#"
+        SELECT *
+        FROM users u
+        JOIN group_members gm ON u.id = gm.user_id
+        WHERE gm.group_id = $1
+        "#,
+        group_id
+    ).fetch_all(&state.db).await;
+
+    match users {
+        Ok(users) => {
+            let users_data = users.iter().map(|u| {
+                json!({
+                    "username": u.username,
+                    "profile_picture": u.profile_picture,
+                    "id": u.id,
+                })
+            }).collect::<Vec<_>>();
+
+            let mut user_in_group = false;
+            for user in users_data.iter() {
+                if user["id"] == user_id {
+                    user_in_group = true;
+                    break;
+                }
+            }
+            if !user_in_group {
+                return (StatusCode::UNAUTHORIZED,Json(json!({ "error": "User not in group" }))).into_response()
+            }
+
+            (StatusCode::OK, Json(users_data)).into_response()
+        }
+        Err(_) => {
+            (StatusCode::INTERNAL_SERVER_ERROR,Json(json!({ "error": "Failed to fetch users" }))).into_response()
         }
     }
 }
