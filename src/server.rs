94a91fe49512cc6a3564388528848026adee5250
leonascim <leonascim21@gmail.com>
2025-03-25 21:36:27 -0400

    update port

--- src/server.rs
+++ src/server.rs
@@ -128,7 +128,7 @@ async fn main() {
         .layer(Extension(auth))
         .with_state(state);
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3002));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("Server running on {}", addr);
     let listener = TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
