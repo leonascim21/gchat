f6b0652af4a6d9be89490b46fb70b997c25dba28
leonascim <leonascim21@gmail.com>
2025-04-19 21:57:14 -0400

    settings form

--- client/src/app/_components/settingsModal.tsx
+++ client/src/app/_components/settingsModal.tsx
@@ -16,12 +16,51 @@ import { User } from "../fetchData";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "@tanstack/react-form";
+import { generateProfilePictureSVG } from "../utils";
+import { useState } from "react";
+import { Label } from "@/components/ui/label";
+
+const userSchema = z.object({
+  email: z.string().email(),
+  username: z.string(),
+  profilePicture: z.string(),
+  currentPassword: z.string(),
+  newPassword: z.string(),
+});
 
 interface Props {
   user: User;
 }
 
 export default function SettingsModal({ user }: Props) {
+  const [profilePicture, setProfilePicture] = useState(user.profile_picture);
+  const [username, setUsername] = useState(user.username);
+
+  const form = useForm({
+    defaultValues: {
+      email: user.email,
+      username: user.username,
+      profilePicture: user.profile_picture,
+      currentPassword: "",
+      newPassword: "",
+    },
+    validators: {
+      onChange: userSchema,
+    },
+    onSubmit: async (e) => {
+      const payload = {
+        email: e.value.email,
+        username: e.value.username,
+        profilePicture: e.value.profilePicture,
+        currentPassword: e.value.currentPassword,
+        newPassword: e.value.newPassword,
+      };
+      console.log(payload);
+    },
+  });
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -41,36 +80,104 @@ export default function SettingsModal({ user }: Props) {
         <DialogDescription className="text-center">
           Manage your account information
         </DialogDescription>
-        <div className="flex flex-col w-full gap-4">
+        <form
+          className="flex flex-col w-full gap-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
+          }}
+        >
           <div className="flex justify-center">
             <div className="relative w-20 h-20">
               <Avatar className="w-full h-full">
-                <AvatarImage src={user.profile_picture} />
+                <AvatarImage src={profilePicture} />
                 <AvatarFallback>
                   {user.username.substring(0, 2).toUpperCase() ?? ""}
                 </AvatarFallback>
               </Avatar>
-              <Dice5 className="absolute bottom-0 right-0 w-7 h-7 hover:cursor-pointer bg-white rounded-full translate-x-1/3 translate-y-1/3 p-1 text-primary" />
+              <form.Field name="profilePicture">
+                {(field) => (
+                  <Dice5
+                    onClick={() => {
+                      const newPfp = generateProfilePictureSVG(username);
+                      setProfilePicture(newPfp);
+                      field.handleChange(newPfp);
+                    }}
+                    className="absolute bottom-0 right-0 w-7 h-7 hover:cursor-pointer bg-white rounded-full translate-x-1/3 translate-y-1/3 p-1 text-primary"
+                  />
+                )}
+              </form.Field>
             </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <p>Email: </p>
-            <Input type="email" required={true} value={user.email} />
-          </div>
-          <div className="flex flex-col gap-1">
-            <p>Username: </p>
-            <Input type="text" required={true} value={user.username} />
-          </div>
-          <div className="flex flex-col gap-1">
-            <p className="text-nowrap">Current Password: </p>
-            <Input type="password" required={true} placeholder="●●●●●●●●" />
-          </div>
-          <div className="flex flex-col gap-1">
-            <p className="text-nowrap">New Password: </p>
-            <Input type="password" required={true} placeholder="●●●●●●●●" />
-          </div>
-          <Button className="max-w-32">Save Changes</Button>
-        </div>
+
+          <form.Field name="email">
+            {(field) => (
+              <div className="flex flex-col gap-1">
+                <Label htmlFor={field.name}>Email</Label>
+                <Input
+                  id={field.name}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  value={field.state.value}
+                />
+              </div>
+            )}
+          </form.Field>
+          <form.Field name="username">
+            {(field) => (
+              <div className="flex flex-col gap-1">
+                <Label htmlFor={field.name}>Username</Label>
+                <Input
+                  id={field.name}
+                  onChange={(e) => {
+                    const newUsername = e.target.value;
+                    field.handleChange(newUsername);
+                    const newPfp = generateProfilePictureSVG(newUsername);
+                    setProfilePicture(newPfp);
+                    form.setFieldValue("profilePicture", newPfp);
+                    setUsername(newUsername);
+                  }}
+                  value={field.state.value}
+                />
+              </div>
+            )}
+          </form.Field>
+          <form.Field name="currentPassword">
+            {(field) => (
+              <div className="flex flex-col gap-1">
+                <Label htmlFor={field.name}>Current Password</Label>
+                <Input
+                  id={field.name}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  value={field.state.value}
+                  placeholder="●●●●●●●●"
+                />
+              </div>
+            )}
+          </form.Field>
+          <form.Field name="newPassword">
+            {(field) => (
+              <div className="flex flex-col gap-1">
+                <Label htmlFor={field.name}>New Password</Label>
+                <Input
+                  id={field.name}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  value={field.state.value}
+                  placeholder="●●●●●●●●"
+                />
+              </div>
+            )}
+          </form.Field>
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+          >
+            {([canSubmit, isSubmitting]) => (
+              <Button type="submit" disabled={!canSubmit} className="max-w-32">
+                {isSubmitting ? "..." : "Save Changes"}
+              </Button>
+            )}
+          </form.Subscribe>
+        </form>
       </DialogContent>
     </Dialog>
   );
